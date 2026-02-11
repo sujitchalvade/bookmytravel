f4a792aa034172a1f8fbf0cdb92d8ae99ef35b59
sujitchalvade <sujitforaws@gmail.com>
2026-02-11 15:18:36 +0530

    hello

---
+++ src/main/java/com/example/bookmytravel/BookmytravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmytravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookmytravel/BookmytravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmytravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
