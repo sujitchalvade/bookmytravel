b8fffd9040c76552669e2268d950afed060b5f18
sujitchalvade <sujitforaws@gmail.com>
2026-02-14 00:43:56 +0530

    to check conflit

--- src/main/java/com/example/bookmytravel/Railways1.java
+++ src/main/java/com/example/bookmytravel/Railways1.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Railways {
     @GetMapping("/railways")
     public String getData() {
-        return "Hello Welcome to BookMyTravel, Please book Railways tickets at 21% discount.";
+        return "Hello Welcome to BookMyTravel, Please book Railways tickets a 21% discount.";
     }
 }
